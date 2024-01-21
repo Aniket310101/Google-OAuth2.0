482be1c2f688d4b244e2adf08eda05970d112a75
Aniket Purkayastha <60284086+Aniket310101@users.noreply.github.com>
2024-01-21 20:42:15 +0530

    Delete dist directory

--- dist/models/user-info.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UserInfoModel = /** @class */ (function () {
-    function UserInfoModel() {
-    }
-    return UserInfoModel;
-}());
-exports.default = UserInfoModel;
