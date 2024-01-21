107bfe2cc94664e554f966fa0b43dfe8a54ad2a9
Aniket Purkayastha <aniketpurkayastha722@gmail.com>
2024-01-21 20:32:30 +0530

    Pre-commit check 7

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import OauthAuthorizationController from './controllers/oauth-authorization.controller';
 import express from 'express';
 
-
 require('dotenv').config();
 const app = express();
 
-app.get('/oauth-authorization', new OauthAuthorizationController().authorizeUser);
+app.get(
+  '/oauth-authorization',
+  new OauthAuthorizationController().authorizeUser,
+);
 app.get('/oauth2callback', new OauthAuthorizationController().generateTokens);
 
 app.listen(3000, () => {
-    console.log('Listening on http://localhost:3000/oauth-authorization?scopes=email,profile');
+  console.log(
+    'Listening on http://localhost:3000/oauth-authorization?scopes=email,profile',
+  );
 });
