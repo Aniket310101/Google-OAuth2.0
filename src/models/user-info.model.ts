f58aeff5b47e95d50571dedcd01336fcac95d6d6
Aniket Purkayastha <60284086+Aniket310101@users.noreply.github.com>
2024-01-21 20:41:16 +0530

    Enhancements. (#1)
    * ESLint Setup. Remove unwanted Packages.
    
    * Nodemon setup.
    
    * Pre-commit check 7
    
    * Pre-commit check 10
    
    * Pre-commit check 11
    
    * ignore dist and setup pre commit hooks
    
    * package.json

--- src/models/user-info.model.ts
+++ src/models/user-info.model.ts
@@ -1,11 +1,11 @@
 export default class UserInfoModel {
-    sub?: string
-    name?: string
-    given_name?: string
-    family_name?: string
-    picture?: string
-    email?: string
-    email_verified?: boolean
-    locale?: string
+    sub?: string;
+    name?: string;
+    given_name?: string;
+    family_name?: string;
+    picture?: string;
+    email?: string;
+    email_verified?: boolean;
+    locale?: string;
 
 }
